23e058f50b952e1f2cbbc8522c0fda54a67d9bff
damkalan <damso.kalansuriya@gmail.com>
2014-05-08 10:16:06 -0400

    data binding

--- EventRegister/JavaScript.js
+++ EventRegister/JavaScript.js
@@ -1,1 +1,8 @@
-﻿angular.module('rootModule', ['ui.bootstrap']);
+﻿var eventRegister = angular.module('EventRegister', ['ui.bootstrap']);
+
+eventRegister.controller("EventRegisterController", function ($scope) {
+    $scope.persons = [
+        { 'FirstName': 'Kolpan', 'LastName': 'Lopertket', 'MiddleInitial' : 'B', 'PhoneNumber':'458-698-756', 'AgeGroup': '2' },
+        { 'FirstName': 'Polod', 'LastName': 'Moerlokuse', 'MiddleInitial': 'F', 'PhoneNumber': '254-657-458', 'AgeGroup': '2' }
+    ];
+});
