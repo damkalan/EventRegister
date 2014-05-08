b9f737bf67ca4bd551f7069b5bf794bb81cd80f9
damkalan <damso.kalansuriya@gmail.com>
2014-05-08 19:04:09 -0400

    adding WebAPI related files

---
+++ EventRegister/EventRegisterController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EventRegister
+{
+    public class EventRegisterController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<PersonViewModel> Get()
+        {
+            List<PersonViewModel> persons = new List<PersonViewModel> {
+                new PersonViewModel{
+                    FirstName = "Kolpan",
+                    LastName = "Lopertkety",
+                    MiddleInitial = "B",
+                    PhoneNumber = "458-698-756",
+                    AgeGroup = "2"
+                },
+
+                new PersonViewModel{
+                    FirstName = "Polod",
+                    LastName = "Moerlokusez",
+                    MiddleInitial = "F",
+                    PhoneNumber = "254-657-458",
+                    AgeGroup = "2"                        
+                }
+            };
+
+            return persons;
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ EventRegister/PersonViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventRegister
+{
+    public class PersonViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleInitial { get; set; }
+        public string PhoneNumber { get; set; }
+        public string AgeGroup { get; set; }
+    }
+}
