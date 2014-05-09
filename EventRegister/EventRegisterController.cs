de2ff2278b41bb12fd17265f580740c5e4f37ce8
damkalan <damso.kalansuriya@gmail.com>
2014-05-08 20:32:46 -0400

    refactor to move service methods

--- EventRegister/EventRegisterController.cs
+++ EventRegister/EventRegisterController.cs
@@ -9,29 +9,12 @@ namespace EventRegister
 {
     public class EventRegisterController : ApiController
     {
+        EventRegisterService eventRegisterService = new EventRegisterService();
+
         // GET api/<controller>
         public IEnumerable<PersonViewModel> Get()
         {
-            List<PersonViewModel> persons = new List<PersonViewModel> {
-                new PersonViewModel{
-                    FirstName = "Kolpan",
-                    LastName = "Lopertkety",
-                    MiddleInitial = "B",
-                    PhoneNumber = "458-698-756",
-                    AgeGroup = "2"
-                },
-
-                new PersonViewModel{
-                    FirstName = "Polod",
-                    LastName = "Moerlokusez",
-                    MiddleInitial = "F",
-                    PhoneNumber = "254-657-458",
-                    AgeGroup = "2"                        
-                }
-            };
-
-            return persons;
-            //return new string[] { "value1", "value2" };
+            return eventRegisterService.getPersonList();
         }
 
         // GET api/<controller>/5
